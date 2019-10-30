6287cdda0f6276e9456463c85cc6fb47a1fd8367
marutomix <40417726+marutomix@users.noreply.github.com>
2019-10-30 23:44:47 +0700

    change access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'txOP8yW8b9zFXTzp3QyaT1NXx5cqoRd9qf97azhgrja+dj5nBTQlhXRZTYIfAsEYzQALhdXTJPlAfMFeZBVyeNZ9pxXSQXfmxyI/VvF4p2eUMtNRezON9EQZml1dBxRXd1LocaqnYcGUDP7c3KNnUgdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
